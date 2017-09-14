f50a8376ba3e16b5ebb89964f275f4d745968d7e
Nick0603 <b10303008@gmail.com>
2017-09-15 05:36:15 +0800

    create inital project

---
+++ chrome_extension/background.js
@@ -0,0 +1,15 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.executeScript(null, {file: "jquery/jquery-2.1.4.js"});
+    chrome.tabs.executeScript(null, {file: "bootstrap/js/bootstrap.min.js"});
+    chrome.tabs.insertCSS(null, {file: "bootstrap/css/bootstrap.min.css"});
+    chrome.tabs.insertCSS(null, {file: "bootstrap/css/bootstrap-theme.min.css"});
+
+    chrome.tabs.executeScript(null, {code: " var course = [];"});
+    chrome.tabs.executeScript(null, {file: "course_data/English103.json"});
+    chrome.tabs.executeScript(null, {file: "course_data/common103.json"});
+    chrome.tabs.executeScript(null, {file: "course_data/ME103.json"});
+
+
+    chrome.tabs.executeScript(null, {file: "content.js"});
+});
+
